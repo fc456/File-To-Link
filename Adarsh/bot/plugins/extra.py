a56c99580c72ed3bb371317fb53e4a5438140eac
fc456 <153304853+fc456@users.noreply.github.com>
2024-04-22 15:32:31 +0530

    Update extra.py

--- Adarsh/bot/plugins/extra.py
+++ Adarsh/bot/plugins/extra.py
@@ -50,7 +50,7 @@ async def follow_user(b,m):
                     disable_web_page_preview=True)
         
 
-@StreamBot.on_message(filters.regex("DC"))
+@StreamBot.on_message(filters.regex("dc"))
 async def start(bot, update):
     text = START_TEXT.format(update.from_user.dc_id)
     await update.reply_text(
